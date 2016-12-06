e511e5c1ffa84c9f607ff8c514ddb0db5e6f625b
An Giang <vinhaz1204@gmail.com>
2016-12-06 17:00:59 +0700

    add micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :microposts
+  validates :name, presence: true
+  validates_presence_of :email
+  validates :name, length: {in: 2..20}
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end
